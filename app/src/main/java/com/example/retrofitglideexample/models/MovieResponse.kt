5b1295078be8036b454d3768ab7946bb99f3237d
Evan <ciriel@live.com>
2021-08-12 09:59:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/retrofitglideexample/models/MovieResponse.kt
@@ -0,0 +1,13 @@
+package com.example.retrofitglideexample.models
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MovieResponse(
+    @SerializedName("results")
+    val movies : List<Movie>
+
+) : Parcelable {
+}
